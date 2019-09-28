d63dd1545fd2e2a6449841eea725b174ddce647a
Robert Lee <bobbydwise1@hotmail.com>
2019-09-27 19:36:12 -0700

    Add starter function to compare magnitude.

--- scripts.js
+++ scripts.js
@@ -10,7 +10,30 @@ The input list is not necessarily ordered in any way.
 For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], you should return [(1, 3), (4, 10), (20, 25)].
 */
 
+const condense = (yourMatrix) => {
+  let output = []
+  for ( i = 0; i < yourMatrix.length; i++ ) {
+    for ( j = 0; j < yourMatrix.length; j++ ) {
+      if ( yourMatrix[i] == yourMatrix[j] ) {
+        continue;
+      }
+      console.log(i,j)
+      console.log(yourMatrix[i], yourMatrix[j])
+      if (yourMatrix[i][1] > yourMatrix[j][0]) {
+        console.log('bigger')
+      } else {
+        console.log('smaller')
+      }
 
+    }
+  }
+}
+
+const interval1 = [1,3]
+const interval2 = [5,8]
+const interval3 = [4,10]
+const interval4 = [20,25]
+let test1 = [interval1, interval2, interval3, interval4]
 
 $(document).ready(function() {
   $('#form-1').submit(function(){
